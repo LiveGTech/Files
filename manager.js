92692135a64c57dc56a8a6cbb1750773becfa700
James-Livesey <jamesliveg@gmail.com>
2024-10-15 20:16:55 +0100

    Refactor provider page management

--- manager.js
+++ manager.js
@@ -35,12 +35,207 @@ var SkeletonTableHeaderCell = astronaut.component("SkeletonTableHeaderCell", fun
     );
 });
 
+export var ProviderPage = astronaut.component("ProviderPage", function(props, children, inter) {
+    var page = Page() (
+        Section (
+            Heading() (props.provider.name)
+        )
+    );
+
+    inter.render = async function() {
+        var entryDisplayName = props.provider.entryName ?? props.provider.name;
+
+        // Create skeleton loader
+
+        page.clear().add(
+            Section({mode: "wide"}) (
+                Heading(1) (entryDisplayName),
+                SkeletonLoader({alt: _("loadingEntries")}) (
+                    Table({
+                        styles: {
+                            "width": "100%"
+                        }
+                    }) (
+                        TableHeader (
+                            TableRow (
+                                SkeletonTableHeaderCell({width: "55%"}) (),
+                                SkeletonTableHeaderCell({width: "15%"}) (),
+                                SkeletonTableHeaderCell({width: "15%"}) (),
+                                SkeletonTableHeaderCell({width: "15%"}) ()
+                            )
+                        ),
+                        TableMain (
+                            ...astronaut.repeat(10, TableRow (
+                                ...astronaut.repeat(4, TableCell() (
+                                    TextFragment({
+                                        attributes: {
+                                            "aui-skeletontext": true
+                                        },
+                                        styles: {
+                                            "width": "100%"
+                                        }
+                                    }) ()
+                                ))
+                            ))
+                        )
+                    )
+                )
+            )
+        );
+
+        page.emit("setcurrentfolder", {name: entryDisplayName});
+
+        // Try mounting provider if not already mounted
+
+        if (!props.provider.isMounted) {
+            try {
+                await props.provider.mount();
+            } catch (error) {
+                console.warn(error);
+
+                var tryAgainButton = Button() (_("tryAgain"));
+
+                tryAgainButton.on("click", function() {
+                    inter.render();
+                });
+
+                page.clear().add(
+                    Section (
+                        Message (
+                            Icon("error", "dark embedded") (),
+                            Heading(1) (_("places_mountError_title")),
+                            Paragraph() (_("places_mountError_description")),
+                            tryAgainButton
+                        )
+                    )
+                );
+
+                return;
+            }
+        }
+
+        // Get folder entries
+
+        var currentEntry = await props.provider.access();
+
+        if (currentEntry.type != "folder") {
+            throw new Error("Not implemented");
+        }
+
+        var listItems = {};
+        var entries = {};
+        var collator = files.currentLocale.createCollator();
+
+        var sortedList = currentEntry.list.sort(function(a, b) {
+            if (a instanceof fs.FolderEntry && b instanceof fs.FileEntry) {
+                return -1;
+            }
+
+            if (a instanceof fs.FileEntry && b instanceof fs.FolderEntry) {
+                return 1;
+            }
+
+            return collator.compare(a.name, b.name);
+        });
+
+        // Transform entries into `ListView` format
+
+        for await (var entry of sortedList) {
+            var nameParts = entry.name.split(".");
+            var displayType = entry instanceof fs.FolderEntry ? _("prop_type_folder") : _("prop_type_file");
+
+            if (entry instanceof fs.FileEntry && nameParts.length > 1) {
+                if (nameParts[0] == "") {
+                    displayType = _("prop_type_hiddenFile");
+                } else {
+                    displayType = _("prop_type_fileWithExtension", {extension: nameParts.at(-1).toLocaleUpperCase()});
+                }
+            }
+
+            if (entry instanceof fs.FolderEntry && nameParts.length > 1 && nameParts[0] == "") {
+                displayType = _("prop_type_hiddenFolder");
+            }
+
+            var size = await entry.getSize();
+            var displaySize = "";
+
+            if (size != null) {
+                displaySize = sizeUnits.getString(size, _);
+            }
+
+            var lastModified = await entry.getLastModified();
+            var displayLastModified = "";
+
+            if (lastModified != null) {
+                // TODO: Display as relative time (implement in Adapt UI, like how `sizeUnits` is)
+                displayLastModified = _format(lastModified);
+            }
+
+            entries[entry.name] = entry;
+
+            listItems[entry.name] = {
+                name: entry.name,
+                displayType,
+                displaySize,
+                displayLastModified
+            };
+        }
+
+        var listView = listViews.ListView({
+            mode: "truncate",
+            items: listItems,
+            keyOrder: ["name", "displayType", "displaySize", "displayLastModified"]
+        }) (
+            TableHeaderCell({
+                mode: "resize",
+                styles: {
+                    "width": "100%"
+                }
+            }) (_("prop_name")),
+            TableHeaderCell({mode: "resize"}) (_("prop_type")),
+            TableHeaderCell({mode: "resize"}) (_("prop_size")),
+            TableHeaderCell({mode: "resize"}) (_("prop_lastModified"))
+        );
+
+        listView.on("activaterow", function(event) {
+            var entry = entries[event.detail.key];
+
+            if (entry instanceof fs.FolderEntry) {
+                props.provider.visit(event.detail.key);
+
+                inter.render();
+            }
+        });
+
+        page.clear().add(
+            Section({mode: "wide"}) (
+                Heading(1) (entryDisplayName),
+                listView
+            )
+        );
+    };
+
+    inter.up = function() {
+        props.provider.up();
+
+        inter.render();
+    };
+
+    inter.visit = function() {
+        inter.render();
+
+        return page.pageFade();
+    };
+
+    return page;
+});
+
 export var ManagerScreen = astronaut.component("ManagerScreen", function(props, children) {
     var backButton = IconButton("back", _("back")) ();
     var headerCurrentFolderName = TextFragment() (_("files"));
     var pageMenuButtonContainer = Container() ();
 
-    var providerInfo = [];
+    var currentProviderPage = null;
 
     var screen = Screen(props) (
         Header (
@@ -69,213 +264,34 @@ export var ManagerScreen = astronaut.component("ManagerScreen", function(props, 
         )
     );
 
-    var currentBackAction = function() {};
-
     backButton.on("click", function() {
-        currentBackAction();
+        currentProviderPage?.inter.up();
     });
 
-    // TODO: Make this a component
     function addProvider(provider) {
-        var page = Page() (
-            Section (
-                Heading() (provider.name)
-            )
-        );
-
+        var page = ProviderPage({provider}) ();
         var pageMenuButton = PageMenuButton({page}) (provider.name);
 
-        async function render() {
-            var entryDisplayName = provider.entryName ?? provider.name;
-
-            page.clear().add(
-                Section({mode: "wide"}) (
-                    Heading(1) (entryDisplayName),
-                    SkeletonLoader({alt: _("loadingEntries")}) (
-                        Table({
-                            styles: {
-                                "width": "100%"
-                            }
-                        }) (
-                            TableHeader (
-                                TableRow (
-                                    SkeletonTableHeaderCell({width: "55%"}) (),
-                                    SkeletonTableHeaderCell({width: "15%"}) (),
-                                    SkeletonTableHeaderCell({width: "15%"}) (),
-                                    SkeletonTableHeaderCell({width: "15%"}) ()
-                                )
-                            ),
-                            TableMain (
-                                ...astronaut.repeat(10, TableRow (
-                                    ...astronaut.repeat(4, TableCell() (
-                                        TextFragment({
-                                            attributes: {
-                                                "aui-skeletontext": true
-                                            },
-                                            styles: {
-                                                "width": "100%"
-                                            }
-                                        }) ()
-                                    ))
-                                ))
-                            )
-                        )
-                    )
-                )
-            );
-
-            headerCurrentFolderName.setText(entryDisplayName);
-
-            if (!provider.isMounted) {
-                try {
-                    await provider.mount();
-                } catch (error) {
-                    console.warn(error);
-
-                    var tryAgainButton = Button() (_("tryAgain"));
-
-                    tryAgainButton.on("click", function() {
-                        render();
-                    });
-
-                    page.clear().add(
-                        Section (
-                            Message (
-                                Icon("error", "dark embedded") (),
-                                Heading(1) (_("places_mountError_title")),
-                                Paragraph() (_("places_mountError_description")),
-                                tryAgainButton
-                            )
-                        )
-                    );
-
-                    return;
-                }
-            }
-
-            var currentEntry = await provider.access();
-
-            if (currentEntry.type != "folder") {
-                throw new Error("Not implemented");
-            }
-
-            var listItems = {};
-            var entries = {};
-            var collator = files.currentLocale.createCollator();
-
-            var sortedList = currentEntry.list.sort(function(a, b) {
-                if (a instanceof fs.FolderEntry && b instanceof fs.FileEntry) {
-                    return -1;
-                }
-
-                if (a instanceof fs.FileEntry && b instanceof fs.FolderEntry) {
-                    return 1;
-                }
-
-                return collator.compare(a.name, b.name);
-            });
-
-            for await (var entry of sortedList) {
-                var nameParts = entry.name.split(".");
-                var displayType = entry instanceof fs.FolderEntry ? _("prop_type_folder") : _("prop_type_file");
-
-                if (entry instanceof fs.FileEntry && nameParts.length > 1) {
-                    if (nameParts[0] == "") {
-                        displayType = _("prop_type_hiddenFile");
-                    } else {
-                        displayType = _("prop_type_fileWithExtension", {extension: nameParts.at(-1).toLocaleUpperCase()});
-                    }
-                }
-
-                if (entry instanceof fs.FolderEntry && nameParts.length > 1 && nameParts[0] == "") {
-                    displayType = _("prop_type_hiddenFolder");
-                }
-
-                var size = await entry.getSize();
-                var displaySize = "";
-
-                if (size != null) {
-                    displaySize = sizeUnits.getString(size, _);
-                }
-
-                var lastModified = await entry.getLastModified();
-                var displayLastModified = "";
-
-                if (lastModified != null) {
-                    // TODO: Display as relative time (implement in Adapt UI, like how `sizeUnits` is)
-                    displayLastModified = _format(lastModified);
-                }
-
-                entries[entry.name] = entry;
-
-                listItems[entry.name] = {
-                    name: entry.name,
-                    displayType,
-                    displaySize,
-                    displayLastModified
-                };
-            }
-
-            var listView = listViews.ListView({
-                mode: "truncate",
-                items: listItems,
-                keyOrder: ["name", "displayType", "displaySize", "displayLastModified"]
-            }) (
-                TableHeaderCell({
-                    mode: "resize",
-                    styles: {
-                        "width": "100%"
-                    }
-                }) (_("prop_name")),
-                TableHeaderCell({mode: "resize"}) (_("prop_type")),
-                TableHeaderCell({mode: "resize"}) (_("prop_size")),
-                TableHeaderCell({mode: "resize"}) (_("prop_lastModified"))
-            );
-
-            listView.on("activaterow", function(event) {
-                var entry = entries[event.detail.key];
-
-                if (entry instanceof fs.FolderEntry) {
-                    provider.visit(event.detail.key);
-
-                    render();
-                }
-            });
-
-            page.clear().add(
-                Section({mode: "wide"}) (
-                    Heading(1) (entryDisplayName),
-                    listView
-                )
-            );
-
-            currentBackAction = function() {
-                provider.up();
-
-                render();
-            };
-        }
-
-        function visit() {
-            render();
-
-            return page.pageFade();
-        }
-
-        providerInfo.push({provider, visit});
+        page.on("setcurrentfolder", function(event) {
+            headerCurrentFolderName.setText(event.detail.name);
+        });
 
         pageMenuButton.on("click", function() {
-            visit();
+            page.inter.visit();
         });
+
+        currentProviderPage = page;
 
         screen.add(page);
         pageMenuButtonContainer.add(pageMenuButton);
+
+        return page;
     }
 
     if (common.IS_SYSTEM_APP) {
-        addProvider(new localFsProvider.LocalFilesystem(_("places_internal")));
+        var providerPage = addProvider(new localFsProvider.LocalFilesystem(_("places_internal")));
 
-        providerInfo[0].visit();
+        providerPage.inter.visit();
     } else {
         addProvider(new localFsProvider.LocalFilesystem(_("places_local")));
     }
